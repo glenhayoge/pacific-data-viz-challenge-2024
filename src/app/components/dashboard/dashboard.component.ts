76a253f26d80aeb3741e20b1752bc196f47b618c
Glen Hayoge <glenhayoge@gmail.com>
2024-05-05 09:41:07 +1000

    added pacvisualcomp logo, chart styles

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,6 +9,12 @@ import {
 import { DataService } from 'src/app/services/data.service';
 import { HttpClient } from '@angular/common/http';
 
+interface GroupedData {
+  country: string;
+  year: number;
+  values: number[];
+}
+
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -25,33 +31,74 @@ export type ChartOptions = {
 export class DashboardComponent implements OnInit{
   @ViewChild("chart", {static: false}) chart: ChartComponent | any;
   public chartOptions: Partial<ChartOptions> | any;
+  seriesData!: ApexAxisChartSeries;
+  categories!: number[];
   dataSet: any;
   constructor(private dataService: DataService, private http: HttpClient) {
+  //   this.chartOptions = {
+  //     series: [
+  //       {
+  //         name: "Time Series",
+  //         data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+  //       }
+  //     ],
+  //     chart: {
+  //       height: 350,
+  //       type: "bar"
+  //     },
+  //     title: {
+  //       text: "Test Chart"
+  //     },
+  //     xaxis: {
+  //       categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+  //     }
+  //   };
+  }
+  ngOnInit(): void {
+    // this.http.get('assets/data/gender-wage-gap.json').subscribe(data => {
+    //   this.dataSet = data;
+    //   this.processData(data);
+    //   // console.log('JSON Data:', this.dataSet);
+    // });
+    this.http.get<any[]>('assets/data/gender-wage-gap.json').subscribe(data => {
+      this.processData(data);
+    });
+    
+    
+ 
+  } 
+  processData(data: any[]): void {
+    const groupedData = data.reduce((acc, curr) => {
+      const key = `${curr.country}-${curr.year}`;
+      if (!acc[key]) {
+        acc[key] = {
+          country: curr.country,
+          year: curr.year,
+          values: []
+        };
+      }
+      acc[key].values.push(curr.observation_value);
+      return acc;
+    }, {});
+
+    const groupedValues: GroupedData[] = Object.values(groupedData);
+    this.categories = [...new Set(groupedValues.map(item => item.year))];
+
     this.chartOptions = {
-      series: [
-        {
-          name: "Time Series",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-        }
-      ],
       chart: {
-        height: 350,
-        type: "bar"
+        type: 'line'
       },
-      title: {
-        text: "Test Chart"
-      },
+      series: groupedValues.map(item => ({
+        name: item.country,
+        data: item.values
+      })),
       xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+        categories: this.categories
       }
     };
   }
-  ngOnInit(): void {
-    this.http.get('assets/data/gender-wage-gap.json').subscribe(data => {
-      this.dataSet = data;
-      // console.log('JSON Data:', this.dataSet);
-    });
-  } 
+  
+  
   }
   
   
