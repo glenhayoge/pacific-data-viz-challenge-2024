46de5b46a8bf979ec494dd24e3d1816f028484d8
Glen Hayoge <glenhayoge@gmail.com>
2024-07-13 15:29:44 +1000

    adjusted svgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media',
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
     "./node_modules/flowbite/**/*.js" 
